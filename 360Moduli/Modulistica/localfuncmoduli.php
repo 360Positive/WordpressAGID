a6b42844f1a9811d2aca986b2314240e6013dbb9
360Positive <riccardotesta@titansolution.it>
2019-09-19 16:29:37 +0200

    Gestione Modulistica

---
+++ 360Moduli/Modulistica/localfuncmoduli.php
@@ -0,0 +1,88 @@
+<?php
+
+
+class XMLINTERPRETERMODULISTICA
+{
+    protected $module_voices = array();
+    protected $id = 0;
+
+    public function init($xmlurl)
+    {
+        $local = site_url();
+              /**
+             * Lettura dei file presenti nella cartelle e sotto directory
+             */
+            $dirs = array_filter(glob('wp-content/themes/design-italia-child/360Moduli/Modulistica/Moduli/*'), 'is_dir');
+            //print_r( $dirs);
+
+            foreach ($dirs as $dir){
+                //echo '<br><br>Letura dir: -<<br>'.$dir.'<br>';
+                $exp=explode('/',$dir);
+                $el=count($exp); //Numero di elementi presenti
+                $dir_name=$exp[$el-1]; //Estrazione del nome della directory
+                //echo '<br><br>Nome dir: -<<br>'.$dir_name.'<br>';
+                $files=glob($dir.'/*');
+                //echo '<br><br>Files: -<<br>'.print_r($files).'<br>';
+                array_push($this->module_voices,["folder"=>$dir_name,"file"=>$files]);
+            }
+
+
+            //echo '<br><br><br>Struttura<br>';
+            //print_r($this->module_voices);
+    }
+
+
+    public function createLayout()
+    {
+        echo '<table id="moduletable" class="table table-striped" style="width: 100%">';
+        echo '<thead>
+                <th>Ufficio</th>
+                <th>Modulo</th>
+              </thead>';
+        echo '<tbody>';
+        foreach ($this->module_voices as $voci) {
+            $ufficio= $voci['folder'];
+
+            foreach ($voci['file'] as $file) {
+                $this->id += 1;
+                $id = $this->id;
+                echo '<tr>';
+                echo '<td>';
+                echo '<strong>'.$ufficio.'</strong>';
+                echo '</td>';
+                echo '<td>';
+                $ext = pathinfo($file, PATHINFO_EXTENSION);
+                $filename=basename($file);
+                echo '<a href="'.site_url().'/'.$file.'" title="'.$filename.'"><i class="icofont-file-pdf"></i> '.$filename.'</a>';
+                echo '</td>';
+                echo '</tr>';
+
+            }
+        }
+        echo '</tbody>';
+        echo '</table>';
+
+        for ($id = 0; $id < $this->id; $id++) {
+            echo '
+        <script>
+        $(\'#accordion' . $id . '\').click(
+function() {
+    $(\'#collapse' . $id . '\').toggle()
+})
+        </script>';
+
+        }
+
+        echo '<style>
+            .buttonfloat{
+            background: #ffb402;
+            padding: 2%;
+            cursor: pointer;
+            
+            }
+            </style>';
+    }
+
+}
+
+?>
---
+++ template-general-moduli.php
@@ -0,0 +1,129 @@
+<?php
+/*
+    * Template Name: Modulistica - template-general-moduli.php
+    * Template Post Type: post, page
+*/
+require_once '360Moduli/Modulistica/localfuncmoduli.php';
+
+get_header(); ?>
+    <!--single.php -->
+    <style>
+        section.topimage{
+            background:url(<?php echo the_field('big-image');?>);
+            min-height:350px;
+            background-position: center;
+            background-size: cover;
+            padding-bottom:0%!important;
+            margin-top: -25px;
+            margin-bottom:0px;
+        }
+        .over{
+            padding:2%;
+        }
+        #sidebar:after{
+            display:none;
+        }
+        .moreact{
+            background:yellow;
+            padding:1%;
+        }
+        .breadcrumb{
+            margin-top:0px;
+        }
+        .icofont-*{
+            font-size:2em!important;
+        }
+
+    </style>
+    <section class="entry-content thumbnail topimage">
+        <p class="dida"><?php the_post_thumbnail_caption() ?></p>
+
+    </section>
+<?php wppa_breadcrumb(); ?>
+    <div class="container">
+    <div class="row">
+
+        <section id="content" role="main" class="container">
+
+            <div class="row">
+                <div class="col-md-9">
+
+                    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                        <?php //get_template_part( 'entry' ); ?>
+                        <h2><?php the_title(); ?></h2>
+                        <hr>
+                        <?php the_content(); ?>
+                        <?php //if ( ! post_password_required() ) comments_template( '', true ); ?>
+                    <?php endwhile; endif; ?>
+
+
+                            <?php
+                            $xmi=new XMLINTERPRETERMODULISTICA();
+                            //get_field('xml')
+                            $xmi->init();
+                            $xmi->createLayout();
+
+                            ?>
+
+
+
+
+                </div>
+
+                <div class="col-md-2 offset-md-1">
+                    <?php //Inclusione modulo per la gestione dei social
+                    include '360Moduli/sharesocial.php';?>
+                    <?php wp_nav_menu(array('menu'=>'"'.get_field('menusidebar').'""')); ?>
+                </div>
+
+
+            </div>
+
+
+        </section>
+    </div>
+    <script>
+        <?php /*---  Script datatable per il raggruppamnento dei dati   ----*/?>
+        $(document).ready( function () {
+            var groupColumn = 0;
+
+            var table = $('#moduletable').DataTable({
+
+                "columnDefs": [
+                    { "visible": false, "targets": groupColumn }
+                ],
+                "order": [[ groupColumn, 'asc' ]],
+                "displayLength": 25,
+                "drawCallback": function ( settings ) {
+                    var api = this.api();
+                    var rows = api.rows( {page:'current'} ).nodes();
+                    var last=null;
+
+                    api.column(groupColumn, {page:'current'} ).data().each( function ( group, i ) {
+                        if ( last !== group ) {
+                            $(rows).eq( i ).before(
+                                '<tr class="group"><td colspan="2">'+group+'</td></tr>'
+                            );
+
+                            last = group;
+                        }
+                    } );
+                },/**/
+              } );
+
+
+        // Order by the grouping
+        $('#faqtable tbody').on( 'click', 'tr.group', function () {
+            var currentOrder = table.order()[0];
+            if ( currentOrder[0] === groupColumn && currentOrder[1] === 'asc' ) {
+                table.order( [ groupColumn, 'desc' ] ).draw();
+            }
+            else {
+                table.order( [ groupColumn, 'asc' ] ).draw();
+            }
+        } );
+        } );
+    </script>'
+
+
+<?php get_footer(); ?>
