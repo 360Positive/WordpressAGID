f377d5148d783be2cd920690ea97d65b803938a6
360Positive <riccardotesta@titansolution.it>
2019-09-20 11:42:20 +0200

    Aggiunta controllo menu sidebar visualizzazione.

--- template-general-faq.php
+++ template-general-faq.php
@@ -46,7 +46,7 @@ get_header(); ?>
         <section id="content" role="main" class="container">
 
             <div class="row">
-                <div class="col-md-9">
+                <div class="col-md-8">
 
                     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                         <?php //get_template_part( 'entry' ); ?>
@@ -66,10 +66,17 @@ get_header(); ?>
 
                 </div>
 
-                <div class="col-md-2 offset-md-1">
-                    <?php //Inclusione modulo per la gestione dei social
-                    include '360Moduli/sharesocial.php';?>
-                    <?php wp_nav_menu(array('menu'=>'"'.get_field('menusidebar').'""')); ?>
+                <div class="col-md-3 offset-md-1">
+                    <div class="container-fluid widget-area page-widget-area">
+                        <?php //Inclusione modulo per la gestione dei social
+                        include '360Moduli/sharesocial.php';?>
+
+                        <?php
+                        $val=get_field('menusidebar');
+                        if($val){
+                            wp_nav_menu(array('menu'=>'"'.$val.'""'));
+                        } ?>
+                    </div>
                 </div>
 
 
