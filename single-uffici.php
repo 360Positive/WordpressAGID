defc98d614fbb77152ee95a068ef4b29dd6ec45a
360Positive <riccardotesta@titansolution.it>
2019-10-17 18:37:00 +0200

    Corretto bottoni e inserito quadratino grigio nella grafica.

--- single-uffici.php
+++ single-uffici.php
@@ -55,7 +55,7 @@ get_header(); ?>
     <div class="row">
         <section id="content" role="main" class="container">
             <div class="row">
-                <div class="col-md-8">
+                <div class="col-md-9">
 
                     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                         <?php //get_template_part( 'entry' ); ?>
@@ -133,7 +133,7 @@ get_header(); ?>
                     </div>
                 </div>
 
-                <div class="col-sm-3 offset-sm-1">
+                <div class="col-sm-3">
                     <div class="container-fluid widget-area page-widget-area">
                         <?php //Inclusione modulo per la gestione dei social
                         include '360Moduli/sharesocial.php'; ?>
@@ -168,17 +168,16 @@ get_header(); ?>
             $('div > p:nth-child(1)', el).hide()
             $('div > p:nth-child(1)', el).append('<hr>')
 
-            var button = "<a class=\"btn btn-lg btn-block text-wrap\" " +
+            var button = "<a class=\"text-left btn angle btn-lg btn-block text-wrap\" style=\"padding-right: 0px; border-radius: 0; \"" +
                 "data-toggle=\"collapse\" " +
                 "href=\"#sec" + index + "\" " +
                 "role=\"button\" " +
                 "aria-expanded=\"false\" " +
                 "aria-controls=\"collapseExample\"" +
-                ">" + title +
-                "<span class=\"pull-right\">\n" +
-                "        <span class=\"icofont-rounded-down\">\n" +
-                "        </span>\n" +
-                "      </span>"+
+                ">"+ title +
+                "        <span class=\"icofont-rounded-down pull-right\" style=\"padding:1%; background: darkgrey; margin-top: -1%; \">" +
+                "        </span>" +
+                "</div>"+
                 "<\/a> ";
 
             $(el).before(button)
