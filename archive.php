f2f465e9acbf944275cb1d79fb19557cb623a112
rikitesta88 <riccardotesta@revilla.it>
2020-05-21 10:24:15 +0200

    Correzione menu navigazione pagina aggironamenti FAQ

--- archive.php
+++ archive.php
@@ -5,21 +5,23 @@ include "360Moduli/Trasparenza/normativa.php";
 $normativa = new Normativa('http://comune.acquiterme.al.it/sviluppo/wp-content/themes/design-italia-child/360Moduli/Trasparenza/servizi.xml');
 
 ?>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.js"></script>
-<style>
-    h2.entry-title {
-        background: lightgray;
-        padding:1%;
-        display:block!important;
-        font-size:1.5rem!important;
-    }
-</style>
+<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>   
+<script type="text/javascript">
+	$('<link/>', {
+		rel: 'stylesheet',
+		type: 'text/css',
+		href: '<?= get_site_url() ?>/wp-content/themes/design-italia-child/360Moduli/css/wp/archive.css'//css da includere
+	}).appendTo('head');
+	
+</script> 
+
 <!-- Archivio archivio.php -->
+<?php pa360_breadcrumb(); ?>
     <section id="content" role="main" class="container">
         <div class="container">
 
             <div class="row">
-                <div class="col-sm-9">
+                <div class="col-sm-9 pr-4">
 
                     <header class="header">
 <!--                        <h1>--><?php
@@ -34,25 +36,49 @@ $normativa = new Normativa('http://comune.acquiterme.al.it/sviluppo/wp-content/t
 //                            }
 //                            ?><!--</h1>-->
                     </header>
-                    <h2 class="entry-title"><?= substr(get_the_archive_title(), 9);?></h2>
+                    <h1 class="entry-title"><?= substr(get_the_archive_title(), 9);?></h1>
                     <?php
 
                     //Estrazione della normativa corrispondente.
-//                    print_r($normativa);
-                    $norme = $normativa->searchin($text = str_replace("'",' ',substr(get_the_archive_title(), 9)), "title")[0]->norma;
-                    foreach ($norme as $norma) {
-                        echo '<div class="alert alert-primary" role="alert">';
-                        echo $norma[0];
-                        echo '</div>';
+                    $text = str_replace("'", ' ', substr(get_the_archive_title(), 9));
+                    $subsect = $normativa->searchin($text, "title")[0];
+                    if(!empty($subsect->subsect)){$subsect=$subsect->subsect;}
 
-                    }
-                    //                    echo ($normativa->searchin($text=substr(get_the_archive_title(),9),"title")[0]->norma[0]);
+                    $norme = $subsect->norma;
+                    $links = $subsect->links->link;
 
                     ?>
+                    <script> console.log("<?= print_r($normativa);?>");</script>
+                    <?php
+                    echo '<br>';
+                    echo '<h3 style="font-variant: small-caps;letter-spacing: 1px; font-size: 1.5rem">Riferimenti normativi</h3><hr>';
+                    foreach ($norme as $norma) {
+                        echo $norma[0];
+                        echo '<br>';
+                    }
+                    echo '<br>';
+                    echo '<h3 style="font-variant: small-caps;letter-spacing: 1px; font-size: 1.5rem">Documenti</h3><hr>';
+                    echo '<p class="text-justify">Nella tabella sottostante potrete trovare la documentazione suddivisa per pagine. 
+Oltre che la ricerca nel sito è possibile effettaure la ricerca localmente utilizzando lo spazio <strong>"Cerca"</strong> sottostante, il quale effettuerà un filtro sui contenuti della sezioen aperta.</p>';
+
+                    ?>
+
+                    <div class="row pt-3">
+                        <p><?= !empty($links) ? __("Nella sezione seguente potrete trovare i collegamenti ai portali istituzionali di riferimento.",'360positive') :'';?></p>
+                        <?php foreach ($links as $voci) {
+                            echo '<div class="col-md-6 alert alert-link" role="alert"><a href="' . $voci->dest . '">';
+                            echo strtoupper($voci->title);
+                            echo ' </a></div> ';
+
+
+                        }
+                        ?>
+                    </div>
+
                     <table id="elencoList" class="display" style="width:100%">
                         <thead>
                         <tr>
-                            <th>Documentazione</th>
+                            <th calss="text-center">Documenti e pagine allegate</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -61,45 +87,42 @@ $normativa = new Normativa('http://comune.acquiterme.al.it/sviluppo/wp-content/t
                             <tr>
                                 <td>
                                     <h3 class="big-heading">
-                                        <a href="<?= get_post()->guid; ?>" title="Apre pagina <?= get_the_title(); ?> ">
+                                        <a href="<?= get_post()->guid; ?>" title="Apre pagina <?= get_the_title(); ?> " target="_blank">
                                             <i class="icofont-link"></i> <?= get_the_title(); ?></a>
-                                        <br><small class="text-muted"> Aggiornato:
-                                            <?php
-                                            $timestamp = strtotime(get_post()->post_modified);
-                                            $modify = date('d/m/Y', $timestamp);
-                                            ?>
-                                            <?= $modify; ?></small>
-                                    </h3>
+											
+                                        </h3>
                                     <?php the_excerpt(); ?>
                                 </td>
                             </tr>
                         <?php endwhile; endif; ?>
                         </tbody>
                     </table>
-
-
+<br><br>
+<p class="text-justify">I dati personali pubblicati sono riutilizzabili solo alle condizioni previste dalla normativa vigente sul riuso dei dati pubblici (direttiva comunitaria 2003/98/CE e d. lgs. 36/2006 di recepimento della stessa), in termini compatibili con gli scopi per i quali sono stati raccolti e registrati, e nel rispetto della normativa in materia di protezione dei dati personali. Per ulteriori informazioni consulta il sito del <strong><a href="http://www.garanteprivacy.it/web/guest/home/docweb/-/docweb-display/docweb/3134436" target="_blank">Garante per la protezione dei dati personali.</a></strong>
+     </p>
 
                 </div>
-            </div>
+            
             <div class="col-sm-3 ">
-
-            </div>
-
+                <?php echo do_shortcode('[at-head-collapse]') ?>
+      </div>
+</div>
         </div>
 
 
     </section>
     <script type="text/javascript">
 
-        jQuery.noConflict();
-        $(document).ready(function () {
+       
             $.noConflict();
+			jQuery.noConflict();
+			
             $("#elencoList").DataTable({
                 "language": {
                     "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Italian.json"
                 },
                 "pageLength": -1
             });
-        });
+       
     </script>
 <?php get_footer(); ?>
