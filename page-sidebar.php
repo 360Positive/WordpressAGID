6c898230a72d39182b0563c50b2eeefd6f162fa3
rikitesta88 <riccardotesta@revilla.it>
2020-06-03 10:50:19 +0200

    Sezione menu laterale correzioni Caricato script e funzione gestione link realtime

--- page-sidebar.php
+++ page-sidebar.php
@@ -1,51 +1,58 @@
 <?php
-   /* Template Name: Pagina con sidebar - page-sidebar.php
-   Template Post Type: post, page, product*/
-
-   get_header();
+/* Template Name: Pagina con sidebar - page-sidebar.php
+Template Post Type: post, page, product*/
+require_once '360Moduli/XML/localfunc.php';
+require_once '360Moduli/php_utils/utils.php';
+get_header();
 ?>
-<style>
-    .entry-title{
-        background: lightgray;
-        padding:1%;
-        display:block!important;
-        font-size:1.5rem!important;
-    }
-</style>
-<?php wppa_breadcrumb(); ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<section id="content" role="main" class="container">
+    <script type="text/javascript"
+            src="<?= get_site_url() ?>/wp-content/themes/design-italia-child/360Moduli/js/utils.js"></script>
+    <script type="text/javascript">
+        $('<link/>', {
+            rel: 'stylesheet',
+            type: 'text/css',
+            href: '<?= get_site_url() ?>/wp-content/themes/design-italia-child/360Moduli/css/templates/pagesidebar.css'
+        }).appendTo('head');
 
-    <div class="row">
-      <div class="col-md-9">
-          <h1 class="entry-title"><?php the_title(); ?></h1>
-          <?php the_content(); ?>
 
-          <?php
-          //Spampa data e informazioni di aggiornamento della pagina
-          echo _('<br>');
-          echo _('Ultima modifica il: ');
-          the_modified_time('d F Y');
-          ?>
-      </div>
+    </script>
+<?php pa360_breadcrumb(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <section id="content" role="main" class="container">
 
-      <div class="col-sm-3 ">
-   	   		<div class="widget-area page-widget-area" style="padding-top: 0px">
-                <?php //Inclusione modulo per la gestione dei social
-                include '360Moduli/sharesocial.php';?>
+        <div class="row">
+            <div class="col-md-9 pr-5">
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+                <?php the_content(); ?>
+
                 <?php
-                $val=get_field('menusidebar');
-                if($val){
-                wp_nav_menu(array('menu'=>'"'.$val.'""'));
-                } ?>
-   		</div>
+                /*Spampa data e informazioni di aggiornamento della pagina
+                echo _('<br>');
+                echo _('Ultima modifica il: ');
+                the_modified_time('d F Y');*/
+                ?>
+            </div>
 
-		</div>
+            <div class="col-md-3">
+                <?php //Inclusione modulo per la gestione dei social
+                include '360Moduli/sharesocial.php'; ?>
+                <div class="widget-area w-100 mt-0 py-0">
+                    <?php
 
-      </div>
+                    $men = new MenuSide();
+                    $men->sidebarMenuStructure();
+                    $men->sidebarMenuTrasparenza();
 
-</section>
+                    ?>
+                </div>
+            </div>
 
-<?php endwhile; endif; ?>
+        </div>
 
+    </section>
+
+<?php endwhile; endif;
+
+?>
+<script>linkIcon(); //Load function to add icon to links</script>
 <?php get_footer(); ?>
