2796dec28e0e881c3667676c45408afbdf9fa2a3
rikitesta88 <riccardotesta@revilla.it>
2020-10-08 13:57:58 +0200

    Fogli stile moduli e pagine

---
+++ 360Moduli/css/templates/aggiornamenti-faq.php
@@ -0,0 +1,15 @@
+.data{
+    background: rgb(122,184,2);
+    background: linear-gradient(90deg, rgba(122,184,2,1) 0%, rgba(122,184,2,0) 100%);
+   
+
+}
+
+.datagiorno{
+    background: rgb(208,229,246);
+    background: linear-gradient(90deg, rgba(208,229,246,1) 0%, rgba(208,229,246,0) 100%);
+    padding:2%;
+    font-size:1.25rem; 
+    text-transform:uppercase;
+}
+	
