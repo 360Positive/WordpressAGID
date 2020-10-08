b5074f9c6a3cc4854b433409ff461cbc76c3a861
rikitesta88 <riccardotesta@revilla.it>
2020-10-08 13:55:01 +0200

    Footer parametrizzato

---
+++ 360Moduli/configuration/layout.php
@@ -0,0 +1,34 @@
+<?php
+
+
+//Elenco delle variabili di configurazione
+
+$layout=[
+    'pec'=>'',
+    'luogo'=>'Piazza Levi, 12-15011 - Acqui Terme - AL',
+    'mail-header'=>'urp@comune.acquiterme.al.it',
+    'tel-header'=>'0144-0000',
+];
+
+
+//FOOTER -- Personalizzazione del footer
+
+//['nomelink','link',1 interno -0 esterno]
+
+$footer_menu2 = [
+    ['Accesso civico', "/amm-trasparente/accesso-civico/", 1],
+    ['Amministrazione trasparente', '/amministrazione-trasparente/', 1],
+    ['Albo Pretorio', 'http://93.62.191.250/cmsacquiterme/portale/albopretorio/albopretorioconsultazione.aspx?IDNODE=2112', 0],
+    ['Whistleblowing', '/amm-trasparente/whistleblowing', 1],
+    ['Privacy','https://privacy.nelcomune.it/comune.acquiterme.al.it']
+];
+
+$footer_menu3 = [
+    ['Sportello unico attività produttive', "http://www.impresainungiorno.gov.it/web/guest/comune?codCatastale=A052", 0],
+    ['Centrale unica di committenza', '/cuc/', 1],
+    ['Ufficio relazioni pubblico', '/ufficio-relazioni-con-il-pubblico-urp', 1]
+];
+
+$footer_list_point="icofont-arrow-right";
+
+?>
