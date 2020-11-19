8b1fecc8781edc3d94b95223331b70a2c44859c9
rikitesta88 <riccardotesta@revilla.it>
2020-11-19 15:57:05 +0100

    Aggiornamento Pagina bandi di concordo grafica più fluida - Avviso modifiche bando (si vedono entro i 5 gg dalla modifica) - Bandi in scadenza colore giallo per quelli che scadranno entro 10gg
    I termini di scadenza sono insicati con variabili

--- template-bandi-concorso.php
+++ template-bandi-concorso.php
@@ -21,7 +21,116 @@ $category_query_args = array(
 );
 
 $category_query = new WP_Query($category_query_args);
+$scadenza=10;
 
+function printAllegati($entry)
+{ /*    La variabile $allegati deve essere un array */
+    $ext = ['doc' => '<i class="icofont-file-document"></i>',
+        'docx' => '<i class="icofont-file-document"></i>',
+        'pdf' => '<i class="icofont-file-document"></i>',
+        'xml' => '<i class="icofont-file-document"></i>',
+        'jpg' => '<i class="icofont-image"></i>'
+    ];
+
+
+    if (!empty($entry['allegati'])) {
+        echo '<div class="row">';
+        foreach ($entry['allegati'] as $allegato) {
+            $filename = str_replace(get_site_url(), '', $allegato['file']);
+            $filepath = $allegato['file'];
+            //Estrae estensione e icona
+            $fileicon = $ext[explode('.', $filename)[1]];
+            $filenameshow = strtoupper(strtolower($allegato['descrizione']));
+
+            echo sprintf('
+            <div class="col-md-12 p-2 text-left">
+            <a href="%s" target="_blank" class=""style="font-size:1rem;">%s %s </a></div>            
+            ', $filepath, $fileicon, $filenameshow);
+        }
+        echo '</div>';
+    }
+
+}
+
+function printTopBanner($tipo, $entry)
+{
+    $rif = (!empty($entry['rif'])) ? $entry['rif'] : '';
+    $settore = (!empty($entry['settore'])) ? $entry['settore'] : '';
+    $link = $entry['link'];
+    $titolo = $entry['title'];
+
+    echo sprintf(
+        ' <span class="%s p-2 mb-2" style="font-size: 1.2rem; font-variant: small-caps"> %s %s </span> <br>
+     <span class="text-wrap text-right p-2 mb-2 w-100" style="font-size:1rem; text-transform: uppercase; "> 
+     <a class="text-wrap p-2 mb-2 w-100" href="%s" target="_blank">%s</a></span>
+', $tipo, $rif, $settore, $link, $titolo);
+
+
+}
+
+function printSideBanner($entry,$scadenza)
+{
+    /**
+     * Stampa stato del bando controllando la data di scadenza e la data attuale di visualizzazione
+     */
+    $stato = "";
+    $termine = $entry['termine'];
+    $termine = explode("/", $entry['termine']);
+    $termine = join('/', array_reverse($termine));
+    $tipo = "";
+    $style = "font-size: 1.25rem; font-variant: small-caps;";
+//    $limit=10; //Limite di giorni per gli avvisi in scadenza
+$limit=$scadenza;
+
+    if ($termine == null) {
+        $termine = "";
+        $cod = sprintf('<span class="badge badge-success text-wrap p-2 mb-2" style="%s"> aperto </span>', $style);
+        $tipo = 'text-success text-wrap w-100';
+    } else {
+        // Intervallo alla scadenza
+        $intervall =  (strtotime($termine)-time())/(3600*24);
+
+        if ($intervall> $limit) {
+            $stato="badge badge-success";
+            $codh='<span class="%s text-wrap p-2 mb-2" style="%s"> aperto </span>';
+            $cod = sprintf($codh,$stato, $style);
+            $tipo = 'text-wrap w-100';
+        } elseif ($intervall >0 && $intervall< $limit) {
+            $stato="badge badge-warning";
+            $codh='<span class="%s text-wrap p-2 mb-2" style="%s"> aperto </span>';
+            $cod = sprintf($codh,$stato, $style);
+            $tipo = 'text-wrap w-100';
+        } else {
+            $stato="badge badge-danger";
+            $codh='<span class=" %s text-wrap p-2 mb-2" style="%s"> chiuso </span>';
+            $cod = sprintf($codh,$stato, $style);
+            $tipo = 'text-wrap w-100';
+        }
+    }
+    $termine = '<span class="text-primary text-wrap p-2 mt-2" style="font-size: 1rem;">' . $termine . '</span>';
+
+    $htm = sprintf('%s <br> %s ', $cod, $termine);
+
+    echo $htm;
+
+    return $tipo;
+}
+
+function alertModifiy($aggior){
+    $style = "font-size:0.8rem;";
+    $intervall =  (time()-strtotime($aggior))/(3600*24);
+//    echo $intervall;
+
+    if($intervall<20){
+        $stato="text-danger";
+        $codh='<br><span class=" %s text-danger text-wrap p-2 mb-2" style="%s"> Aggiornamento del %s </span>';
+        $cod = sprintf($codh,$stato, $style, $aggior);
+
+        $htm = sprintf('%s', $cod);
+
+        echo $htm;
+    }
+}
 ?>
 <?php pa360_breadcrumb(); ?>
 <script type="text/javascript">
@@ -38,8 +147,25 @@ $category_query = new WP_Query($category_query_args);
         <div class="container">
             <div class="row my-2">
                 <div class="col-md-12"><br>
-                    Utilizza la lo spazio per la ricerca per filtrare le voci che ti interessano.<br>
-                    Per visualizzare più contenuti, aumenta il numero di elementi da visualizzare. I primi elementi sono voci ancora aperte e alle quali si può partecipare. 
+
+
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <strong>Legenda:</strong>
+                            <div class="row">
+                                <div class="col-12"><span class="badge badge-success">Aperti:</span> Bandi attivi e non in scadenza</div>
+                                <div class="col-12"><span class="badge badge-warning">Aperti:</span> In scadenza entro i prossimi <?=$scadenza?> giorni</div>
+                                <div class="col-12"><span class="badge badge-danger">Chiusi:</span> Bandi chiusi</div>
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            Utilizza la lo spazio per la ricerca per filtrare le voci che ti interessano.<br>
+                            Per visualizzare più contenuti, aumenta il numero di elementi da visualizzare. I primi elementi sono
+                            voci ancora aperte e alle quali si può partecipare.
+                        </div>
+                    </div>
+
                     <table id="tablecreate" class="table table-striped w-100">
                         <thead>
                         <tr>
@@ -54,11 +180,6 @@ $category_query = new WP_Query($category_query_args);
 
                         if ($category_query->have_posts()) : while ($category_query->have_posts()): $category_query->the_post();
 
-                            $ext = ['doc' => '<i class="icofont-file-document"></i>',
-                                'pdf' => '<i class="icofont-file-document"></i>',
-                                'xml' => '<i class="icofont-file-document"></i>',
-                                'jpg' => '<i class="icofont-image"></i>'
-                            ];
 
                             $entry = ['title' => get_the_title(),
                                 'rif' => get_field('riferimento'),
@@ -71,68 +192,42 @@ $category_query = new WP_Query($category_query_args);
                                 'link' => get_post_permalink()];
 
                             ?>
-                            <?php
-                            /**
-                             * Stampa stato del bando controllando la data di scadenza e la data attuale di visualizzazione
-                             */
-                            $stato = "";
-                            $termine=$entry['termine'];
-                            $termine=explode("/",$entry['termine']);
-                            $termine=join('/',array_reverse($termine));
-                            $tipo="";
 
-                            if ($termine == null) {
-                                $termine = "Non definito";
-                                $stato = '<span class="badge badge-success text-wrap " style="font-size: 1.5rem; width: 100%;"> Aperto </span>';
-                                $tipo='badge badge-success text-wrap w-100';
-                            } else {
-//                                echo $termine.' ::: '.(time() - strtotime($termine)).'<br>';
-
-                                if(time() - strtotime($termine)<0){
-                                    $stato = '<span class="badge badge-success text-wrap" style="font-size: 1.5rem; width: 100%;"> Aperto </span>';
-                                    $tipo='badge badge-success text-wrap w-100';
-                                }
-                                else {
-                                    $stato = '<span class="badge badge-danger text-wrap" style="font-size: 1.5rem; width: 100%;"> Scaduto </span>';
-                                    $tipo='badge badge-danger text-wrap w-100';
-                                }
-                            }
-                            $termine='<span class="badge badge-primary text-wrap p-2 mt-2" style="font-size: 1.2rem; width: 100%;">'.$termine.'</span>'
-                            ?>
                             <tr>
-                                <td style="width:10%" class="text-center">
-                                    <?= $stato ?>
-                                    <br>
-                                    <?= $termine ?>
-                                    <div class="titled text-center my-2 font-weight-bold" style="display: none">
-                                        <?= $entry['title'] ?>
-                                    </div>
-                                </td>
-                                <td style="width:90%">
-                                    <span class="<?= $tipo?> mb-2" style="font-size: 1.5rem; width: 100%; font-variant: small-caps">
-                                        <?= (!empty($entry['rif'])) ? $entry['rif'] : '' ?>
-                                        <?= (!empty($entry['tipo'])) ? $entry['tipo'] : '' ?>
-                                        <?= (!empty($entry['settore'])) ? $entry['settore'] : '' ?></span><br>
-                                    <span class="badge badge-info text-wrap text-center p-2 mb-2" style="font-size:1.25rem; text-transform: uppercase; width:100%"> <a href="<?= $entry['link'] ?>"><?= $entry['title'] ?> </a></span>
-                                    <br>
-
-                                    <?php the_content(); ?>
-                                    <div class="entry-links"><?php wp_link_pages(); ?></div>
-                                    <hr>
-                                    <strong style="font-size:1.25rem;font-variant:small-caps"><?php echo __('Documentazione relativa'); ?>:</strong><br>
+                                <td style="w-10" class="text-center align-top">
                                     <?php
-                                    // 		print_r($entry);
-                                    if (!empty($entry['allegati'])) {
-                                        foreach ($entry['allegati'] as $allegato) {
-                                            $filename = str_replace(get_site_url(), '', $allegato['file']);
-                                            echo '<span class="badge badge-light p-2 text-left">
-<a href="' . $allegato['file'] . '" target="_blank" class=" text-wrap"style="font-size:1rem;">' . $ext[explode('.', $filename)[1]] . '  ' .strtoupper(strtolower( $allegato['descrizione'])) . '</a>
-
-</span> ';
-                                        }
-                                    }
+                                    //Stampa il banner laterale e ritorna lo stato del bando (aperto chiuso)
+                                    $tipo = printSideBanner($entry,$scadenza) ?>
+                                </td>
+                                <td style="w-90" class="align-top">
+                                    <?php
+                                    //Stampa intestazione bando
+                                    printTopBanner($tipo, $entry);
+                                    $aggior= get_the_modified_date("Y/m/d");
+                                    alertModifiy($aggior);
                                     ?>
 
+                                    <!--                                    <div class="row">-->
+                                    <!--                                        <div class="col-md-8">-->
+                                    <!--                                            <strong style="font-size:1.25rem; font-variant:small-caps">--><?php //echo __('Estratto'); ?>
+                                    <!--                                                :</strong><br>-->
+                                    <!--                                            --><? //= // Stampa solo un estratto dell'articolo
+                                    //                                            the_excerpt(); ?>
+                                    <!--                                        </div>-->
+                                    <!--                                        <div class="col-md-4">-->
+                                    <!--                                            <strong style="font-size:1.25rem; font-variant:small-caps">--><?php //echo __('Documentazione'); ?>
+                                    <!--                                                :</strong><br>-->
+                                    <!--                                            --><?php
+                                    //                                            // Stampa allegati specifico bando
+                                    //                                            printAllegati($entry);
+                                    //                                            ?>
+                                    <!--                                        </div>-->
+                                    <!--                                    <div class="entry-links">-->
+                                    <?php //echo wp_link_pages(); ?><!--</div>-->
+                                    <!--                                                                     Link alla pagina con articolo-->
+                                    <!--                                    <a href="-->
+                                    <? //= get_permalink() ?><!--" class="btn btn-block" target="_blank" title="Apre pagina bando ">Leggi di-->
+                                    <!--                                        più</a>-->
                                 </td>
                             </tr>
 
@@ -147,7 +242,8 @@ $category_query = new WP_Query($category_query_args);
     </section>
     <section id="oldcontent-section">
         <div class="container">
-            <span id="annipre" class="badge badge-danger text-wrap mt-3" style="font-size: 1.5rem; width: 100%; display: none"> Dati anni precedenti </span>
+            <span id="annipre" class="badge badge-danger text-wrap mt-3"
+                  style="font-size: 1.5rem; width: 100%; display: none"> Dati anni precedenti </span>
 
             <div class="row">
 
@@ -247,7 +343,12 @@ $category_query = new WP_Query($category_query_args);
             "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Italian.json"
         },
         responsive: true,
-        "order": [[0, "asc"]]
+        "order": [[0, "asc"]],
+        aLengthMenu: [
+            [25, 50, 100, 200, -1],
+            [25, 50, 100, 200, "All"]
+        ],
+        iDisplayLength: -1
     });
 
 
@@ -269,24 +370,22 @@ $category_query = new WP_Query($category_query_args);
 <script>
     //COMPORTAMENTO MOBILE DESKTOP
     // alla modifica delle pagine nella versione mobile mostra un info sui contenuti
-    let w_width=$(window).width();
-    let table=$('#tablecreate')
+    let w_width = $(window).width();
+    let table = $('#tablecreate')
 
-    $(window).bind('load',function(){
-        if(w_width <684){
-            $('div.titled',table).show()
-        }
-        else{
-            $('div.titled',table).hide()
+    $(window).bind('load', function () {
+        if (w_width < 684) {
+            $('div.titled', table).show()
+        } else {
+            $('div.titled', table).hide()
         }
         $(document).trigger('change');
     })
-    $(window).bind('resize',function(){
-        if(w_width <684){
-            $('div.titled',table).show()
-        }
-        else{
-            $('div.titled',table).hide()
+    $(window).bind('resize', function () {
+        if (w_width < 684) {
+            $('div.titled', table).show()
+        } else {
+            $('div.titled', table).hide()
         }
         $(document).trigger('change');
     })
